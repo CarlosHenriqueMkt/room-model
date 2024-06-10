48f8157be02fc6120710af15525cc3e8f9fddcc2
carloshenriquemkt <falacom.ocarlos@gmail.com>
2024-06-10 00:38:55 -0300

    initial

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { RoomEnvironment } from 'three/addons/environments/RoomEnvironment.js';
 
 const sizes = {
   width: window.innerWidth,
@@ -7,22 +10,20 @@ const sizes = {
 
 const aspectRatio = sizes.width / sizes.height
 
-let renderer, scene, camera, mesh, geometry, material, canvas;
-const clock = new THREE.Clock()
+let renderer, scene, camera, canvas, controls;
+let loader, environment, pmremGenerator;
+
+/* const clock = new THREE.Clock() */
 
 experience();
+model();
 animate();
 
 function experience() {
   scene = new THREE.Scene();
-  camera = new THREE.PerspectiveCamera(65, aspectRatio, 1, 1000);
-  camera.position.z = 5
+  camera = new THREE.PerspectiveCamera(50, aspectRatio, 0.1, 1000);
+  camera.position.set(-5,2.5,3.5)
   scene.add(camera);
-
-  geometry = new THREE.BoxGeometry(1, 1, 1);
-  material = new THREE.MeshNormalMaterial();
-  mesh = new THREE.Mesh(geometry, material);
-  scene.add(mesh);
 
   window.addEventListener('resize', () => {
     sizes.width = window.innerWidth;
@@ -33,6 +34,7 @@ function experience() {
 
     renderer.setSize(sizes.width, sizes.height);
   });
+
 
   canvas = document.getElementById('app');
   renderer = new THREE.WebGLRenderer(
@@ -45,19 +47,44 @@ function experience() {
   renderer.setSize(sizes.width, sizes.height);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
   canvas.appendChild(renderer.domElement);
+
+  controls = new OrbitControls( camera, renderer.domElement );
+  controls.enableDamping = true
+
+
   renderer.render(scene, camera);
+}
+
+function model() {
+
+  environment = new RoomEnvironment( renderer );
+  pmremGenerator = new THREE.PMREMGenerator( renderer );
+
+  scene.background = new THREE.Color( 0x000000 );
+  scene.environment = pmremGenerator.fromScene( environment ).texture;
+
+  loader = new GLTFLoader();
+
+  loader.load( '/model/room/scene.gltf', function ( gltf ) {
+    
+    const model = gltf.scene
+    scene.add( model );
+    const boundingBox = new THREE.Box3().setFromObject(model);
+    const center = boundingBox.getCenter(new THREE.Vector3());
+    camera.lookAt(center);
+
+  }, undefined, function ( error ) {
+
+    console.error( error );
+
+  } );
+
 }
 
 function animate() {
 
-  const elapsedTime = clock.getElapsedTime()
-
-  const meshMoviment = elapsedTime * 0.5
-
-  mesh.rotation.x = Math.sin(meshMoviment) * 1
-  mesh.rotation.y = Math.sin(meshMoviment) * 1
-  mesh.rotation.z = Math.sin(meshMoviment) * 1
-
+  /* const elapsedTime = clock.getElapsedTime() */
+  controls.update();
   requestAnimationFrame(animate);
   renderer.render(scene, camera);
 }
